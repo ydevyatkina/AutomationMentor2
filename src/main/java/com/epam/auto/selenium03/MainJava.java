1fdf766da84ae838064a097a46307f35f117dfd6
Yulia Devyatkina <yulia.devyatkina@jetbrains.com>
2026-08-31 11:50:18 +0200

    Upon review comment

--- src/main/java/com/epam/auto/selenium03/MainJava.java
+++ src/main/java/com/epam/auto/selenium03/MainJava.java
@@ -3,5 +3,6 @@ package com.epam.auto.selenium03;
 public class MainJava {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("End of program");
     }
 }
